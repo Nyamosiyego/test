409c6e963e6c1944c348260ef908ed8ee9a390b7
Edmond <edmond.nyamosi@strathmore.edu>
2023-09-19 12:17:49 +0300

    .

--- pages/job.js
+++ pages/job.js
@@ -89,12 +89,12 @@ const Jobs = () => {
         }}
       />
       <div className="px-8 py-4 mt-20">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-20">
           {jobs.length > 0 &&
             jobs.map((job) => (
               <div
                 key={job._id}
-                className="mb-8 w-auto h-40 grid grid-flow-row auto-rows-3fr"
+                className="w-auto h-40 grid grid-flow-row auto-rows-3fr"
               >
                 <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                   <Link href={`/jobs/${job._id}`}>
@@ -103,7 +103,7 @@ const Jobs = () => {
                     </h5>
                   </Link>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {job.description}
+                    {job.description.length > 250 ? job.description.substring(0, 250) + "..." : job.description}
                   </p>
                   <div className="flex items-center justify-between">
                     <Link
